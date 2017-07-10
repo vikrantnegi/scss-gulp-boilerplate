367ed0ef8bbf6a9bc50c72de3a43249169064e0f
vikrantnegi <vikrantnegi007@gmail.com>
2017-07-10 17:42:38 +0530

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('default', function () {
             cascade: false
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', function () {
