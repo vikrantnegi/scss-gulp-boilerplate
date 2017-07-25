6ba9070419256210c05f7971c8daa7646b5b1b3b
vikrantnegi <vikrantnegi007@gmail.com>
2017-07-25 11:16:56 +0530

    add gulp-imagemin package

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
+var imagemin = require('gulp-imagemin');
 
 
 gulp.task('default', function () {
@@ -16,6 +17,16 @@ gulp.task('default', function () {
         }))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./css'));
+});
+
+// Optimizing Images
+gulp.task('images', function () {
+    return gulp.src('./img/**/*.+(png|jpg|jpeg|gif|svg)')
+        // Caching images that ran through imagemin
+        .pipe(imagemin({
+            interlaced: true,
+        }))
+        .pipe(gulp.dest('./img'))
 });
 
 gulp.task('watch', function () {
